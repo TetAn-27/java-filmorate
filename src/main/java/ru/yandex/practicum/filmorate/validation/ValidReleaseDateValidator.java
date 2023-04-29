1baf099dc08af47d90b1d239e3ee2e3c16279c90
TetAn <tetan27@yandex.ru>
2023-04-29 11:07:19 +0700

    test: создать тест для проверки валидации параметров класса Film

---
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ValidReleaseDateValidator.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+public class ValidReleaseDateValidator implements ConstraintValidator<ValidReleaseDate, LocalDate> {
+
+    private String message;
+
+    @Override
+    public void initialize(final ValidReleaseDate constraintAnnotation) {
+        this.message = constraintAnnotation.message();
+    }
+
+    @Override
+    public boolean isValid(final LocalDate releaseDate, final ConstraintValidatorContext context) {
+
+        LocalDate date = LocalDate.of(1895, 12, 25);
+
+        boolean isValid = releaseDate == null || releaseDate.isAfter(date);
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(message).addConstraintViolation();
+        }
+
+        return isValid;
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -0,0 +1,68 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.*;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmControllerTest {
+    private final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = factory.getValidator();
+    private Film film;
+
+    @BeforeEach
+    public void beforeEach() {
+        film = new Film();
+    }
+
+    @Test
+    public void testNameWhenNameIsEmpty() {
+        film.setName("");
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(1, violations.size(), "Валидация для параметра name некорректна");
+    }
+
+    @Test
+    public void testNameWhenNameIsNull() {
+        film.setName(null);
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(1, violations.size(), "Валидация для параметра name некорректна");
+    }
+
+    @Test
+    public void testDurationWhenValuesNegative() {
+        film.setName("Avatar");
+        film.setDuration(Duration.ZERO);
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(1, violations.size(), "Валидация для параметра duration некорректна");
+    }
+
+    @Test
+    public void testDescriptionWhenLengthIsMore200() {
+        film.setName("Avatar");
+        film.setDescription("DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+                "DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+                "DescriptionDescription");
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(1, violations.size(), "Валидация для параметра description некорректна");
+    }
+
+    @Test
+    public void testReleaseDate() {
+        film.setName("Avatar");
+        film.setReleaseDate(LocalDate.MIN);
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(1, violations.size(), "Валидация для параметра description некорректна");
+    }
+}
