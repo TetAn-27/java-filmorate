5fb387e1322b39c156f24bb80871a01fdbf9699f
TetAn <tetan27@yandex.ru>
2023-04-30 14:49:30 +0700

    добавил строчку

--- src/main/java/ru/yandex/practicum/filmorate/validation/ValidReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ValidReleaseDateValidator.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.validation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
+
 public class ValidReleaseDateValidator implements ConstraintValidator<ValidReleaseDate, LocalDate> {
 
     private String message;
