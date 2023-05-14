9fab815a1ca91c2bdaa2a5c6ee2f9165e334aa1c
TetAn <tetan27@yandex.ru>
2023-05-14 15:13:26 +0700

    feat: дополнить контроллер FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,31 +1,55 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.service.film.FilmService;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/films")
+@AllArgsConstructor
 public class FilmController {
 
-   InMemoryFilmStorage inMemoryFilmStorage = new InMemoryFilmStorage();
+    private final FilmService filmService;
+
     @GetMapping()
     public List<Film> getAllFilms() {
-        return inMemoryFilmStorage.getAllFilms();
+        return filmService.getAllFilms();
+    }
+
+    @GetMapping("/{id}")
+    public Film getFilmById(@PathVariable("id") Integer id) {
+        return filmService.getFilmById(id);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> getTopFilms(@RequestParam(defaultValue = "10", required = false) Integer count) {
+        return filmService.getTop10Films(count);
     }
 
     @PostMapping()
     public Film postFilm(@Valid @RequestBody Film film) {
-        return inMemoryFilmStorage.postFilm(film);
+        return filmService.postFilm(film);
     }
 
     @PutMapping()
     public Film putFilm(@Valid @RequestBody Film film, HttpServletResponse response) {
-        return inMemoryFilmStorage.putFilm(film, response);
+        return filmService.putFilm(film, response);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void addLike(@PathVariable("id") Integer id, @PathVariable("userId") Integer userId) {
+        filmService.addLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void deleteLike(@PathVariable("id") Integer id, @PathVariable("friendId") Integer friendId) {
+        filmService.deleteLike(id, friendId);
     }
 }
 
