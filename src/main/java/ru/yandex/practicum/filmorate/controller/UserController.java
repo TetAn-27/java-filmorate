ae2e73413780d5b5e5b80bc673761ad24ffbd6d8
TetAn <tetan27@yandex.ru>
2023-05-14 15:13:12 +0700

    feat: дополнить контроллер UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,32 +1,58 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
-
+import ru.yandex.practicum.filmorate.service.user.UserService;
 
 import java.util.List;
+import java.util.Set;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
+@AllArgsConstructor
 public class UserController {
 
-   InMemoryUserStorage inMemoryUserStorage = new InMemoryUserStorage();
+   private UserService userService;
 
     @GetMapping()
     public List<User> getAllUsers() {
-        return inMemoryUserStorage.getAllUsers();
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}/friends")
+    public Set<User> getFriends(@PathVariable("id") Integer id) {
+        return userService.getFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> getMutualFriends(@PathVariable("id") Integer id, @PathVariable("otherId") Integer otherId) {
+        return userService.getListOfMutualFriends(id, otherId);
+    }
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable("id") Integer id) {
+        return userService.getUserById(id);
     }
 
     @PostMapping()
     public User postUser(@Valid @RequestBody User user) {
-        return inMemoryUserStorage.postUser(user);
+        return userService.postUser(user);
     }
 
     @PutMapping()
     public User putUser(@Valid @RequestBody User user, HttpServletResponse response) {
-        return inMemoryUserStorage.putUser(user, response);
+        return userService.putUser(user, response);
+    }
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend(@PathVariable("id") Integer id, @PathVariable("friendId") Integer friendId) {
+        userService.addFriend(id, friendId);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void deleteFriend(@PathVariable("id") Integer id, @PathVariable("friendId") Integer friendId) {
+        userService.deleteFriend(id, friendId);
     }
 }
